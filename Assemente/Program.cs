2bbdaab733bf04d2c109adfc684663332d92826d
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-21 18:23:46 +0300

    Q-2

--- Assemente/Program.cs
+++ Assemente/Program.cs
@@ -3,35 +3,62 @@
     internal class Program
     {
 
-            #region Q-1
-            struct Person
+        #region Q-1
+        //    struct Person
+        //{
+        //    public string Name;
+        //    public int Age;
+        //}
+
+
+        //    static void Main()
+        //    {
+
+        //        Person[] people = new Person[3];
+
+
+        //        people[0].Name = "Ahmed";
+        //        people[0].Age = 22;
+
+        //        people[1].Name = "Salma";
+        //        people[1].Age = 19;
+
+        //        people[2].Name = "Youssef";
+        //        people[2].Age = 25;
+
+
+        //        Console.WriteLine("People Details:");
+        //        for (int i = 0; i < people.Length; i++)
+        //        {
+        //            Console.WriteLine("Name: " + people[i].Name + ", Age: " + people[i].Age);
+        //        }
+        //    }
+        #endregion
+        #region Q-2
+        struct Point
         {
-            public string Name;
-            public int Age;
+            public double X;
+            public double Y;
         }
 
-     
+        
             static void Main()
             {
-             
-                Person[] people = new Person[3];
+                Point p1, p2;
 
-                
-                people[0].Name = "Ahmed";
-                people[0].Age = 22;
+                Console.WriteLine("Enter X and Y for point 1:");
+                p1.X = Convert.ToDouble(Console.ReadLine());
+                p1.Y = Convert.ToDouble(Console.ReadLine());
 
-                people[1].Name = "Salma";
-                people[1].Age = 19;
+                Console.WriteLine("Enter X and Y for point 2:");
+                p2.X = Convert.ToDouble(Console.ReadLine());
+                p2.Y = Convert.ToDouble(Console.ReadLine());
 
-                people[2].Name = "Youssef";
-                people[2].Age = 25;
+                double dx = p2.X - p1.X;
+                double dy = p2.Y - p1.Y;
+                double distance = Math.Sqrt(dx * dx + dy * dy);
 
-                
-                Console.WriteLine("People Details:");
-                for (int i = 0; i < people.Length; i++)
-                {
-                    Console.WriteLine("Name: " + people[i].Name + ", Age: " + people[i].Age);
-                }
+                Console.WriteLine("Distance between the two points is: " + distance);
             }
             #endregion
         }
