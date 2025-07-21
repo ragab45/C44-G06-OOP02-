a7f3474457c59f1d06e7afba1d65f1032c849773
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-21 18:22:41 +0300

    Q-1

--- Assemente/Program.cs
+++ Assemente/Program.cs
@@ -2,9 +2,38 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+            #region Q-1
+            struct Person
         {
-            Console.WriteLine("Hello, World!");
+            public string Name;
+            public int Age;
         }
-    }
+
+     
+            static void Main()
+            {
+             
+                Person[] people = new Person[3];
+
+                
+                people[0].Name = "Ahmed";
+                people[0].Age = 22;
+
+                people[1].Name = "Salma";
+                people[1].Age = 19;
+
+                people[2].Name = "Youssef";
+                people[2].Age = 25;
+
+                
+                Console.WriteLine("People Details:");
+                for (int i = 0; i < people.Length; i++)
+                {
+                    Console.WriteLine("Name: " + people[i].Name + ", Age: " + people[i].Age);
+                }
+            }
+            #endregion
+        }
+    
 }
