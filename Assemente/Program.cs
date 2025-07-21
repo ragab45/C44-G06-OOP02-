2ba058053af3e2f4637eca9e32e78b056d2bfcd3
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-21 18:24:39 +0300

    Q-3

--- Assemente/Program.cs
+++ Assemente/Program.cs
@@ -35,30 +35,64 @@
         //    }
         #endregion
         #region Q-2
-        struct Point
+        //struct Point
+        //{
+        //    public double X;
+        //    public double Y;
+        //}
+
+
+        //    static void Main()
+        //    {
+        //        Point p1, p2;
+
+        //        Console.WriteLine("Enter X and Y for point 1:");
+        //        p1.X = Convert.ToDouble(Console.ReadLine());
+        //        p1.Y = Convert.ToDouble(Console.ReadLine());
+
+        //        Console.WriteLine("Enter X and Y for point 2:");
+        //        p2.X = Convert.ToDouble(Console.ReadLine());
+        //        p2.Y = Convert.ToDouble(Console.ReadLine());
+
+        //        double dx = p2.X - p1.X;
+        //        double dy = p2.Y - p1.Y;
+        //        double distance = Math.Sqrt(dx * dx + dy * dy);
+
+        //        Console.WriteLine("Distance between the two points is: " + distance);
+        //    }
+        #endregion
+        #region Q-3
+        struct Person
         {
-            public double X;
-            public double Y;
+            public string Name;
+            public int Age;
         }
 
-        
+       
             static void Main()
             {
-                Point p1, p2;
+                Person[] people = new Person[3];
 
-                Console.WriteLine("Enter X and Y for point 1:");
-                p1.X = Convert.ToDouble(Console.ReadLine());
-                p1.Y = Convert.ToDouble(Console.ReadLine());
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Enter name of person " + (i + 1) + ":");
+                    people[i].Name = Console.ReadLine();
 
-                Console.WriteLine("Enter X and Y for point 2:");
-                p2.X = Convert.ToDouble(Console.ReadLine());
-                p2.Y = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter age of " + people[i].Name + ":");
+                    people[i].Age = Convert.ToInt32(Console.ReadLine());
+                }
 
-                double dx = p2.X - p1.X;
-                double dy = p2.Y - p1.Y;
-                double distance = Math.Sqrt(dx * dx + dy * dy);
+                Person oldest = people[0];
 
-                Console.WriteLine("Distance between the two points is: " + distance);
+                for (int i = 1; i < 3; i++)
+                {
+                    if (people[i].Age > oldest.Age)
+                    {
+                        oldest = people[i];
+                    }
+                }
+
+                Console.WriteLine("The oldest person is: " + oldest.Name + " (" + oldest.Age + " years old)");
             }
             #endregion
         }
